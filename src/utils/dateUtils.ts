54efae3739a8a60fd49eeb99c05d385bf432ba3a
Anesszereg <anesszereg1@gmail.com>
2026-01-05 13:33:08 +0100

    Fix timezone issue in salary cycle calculation
    - Use Date.UTC() to create dates in UTC timezone
    - Prevents off-by-one day errors in cycle calculation
    - Ensures employee hired on 24th gets cycle from 24th to 23rd
    - Fixes issue where remaining salary wasn't resetting for new cycle

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -10,14 +10,14 @@ export function getEmployeeSalaryCycle(hireDate: Date, referenceDate: Date = new
   let cycleStartDate: Date;
   
   if (refDay < hireDayOfMonth) {
-    cycleStartDate = new Date(refYear, refMonth - 1, hireDayOfMonth, 0, 0, 0, 0);
+    cycleStartDate = new Date(Date.UTC(refYear, refMonth - 1, hireDayOfMonth, 0, 0, 0, 0));
   } else {
-    cycleStartDate = new Date(refYear, refMonth, hireDayOfMonth, 0, 0, 0, 0);
+    cycleStartDate = new Date(Date.UTC(refYear, refMonth, hireDayOfMonth, 0, 0, 0, 0));
   }
   
   const nextCycleStart = addMonths(cycleStartDate, 1);
   const cycleEndDate = addDays(nextCycleStart, -1);
-  cycleEndDate.setHours(23, 59, 59, 999);
+  cycleEndDate.setUTCHours(23, 59, 59, 999);
   
   return {
     start: cycleStartDate,
